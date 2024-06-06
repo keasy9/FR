6c2fdd84e757a8023414686ee87a14c228acb4d2
keasy9 <keasy9@gmail.com>
2024-06-06 12:40:28 +0700

    fixup! Чистка пользователей

--- db_migrations/20240606-delete-wp_users.sql
+++ db_migrations/20240606-delete-wp_users.sql
@@ -1,5 +1,5 @@
-DELETE FROM wp_users, wp_usermeta
+DELETE wp_users, wp_usermeta
 FROM wp_users INNER JOIN wp_usermeta ON wp_users.ID = wp_usermeta.user_id
 WHERE wp_usermeta.meta_key = 'wp_capabilities' AND
-    wp.usermeta meta_value NOT LIKE '%administrator%' AND
-    wp.usermeta meta_value NOT LIKE '%fr_moderator%'
+    wp_usermeta.meta_value NOT LIKE '%administrator%' AND
+    wp_usermeta.meta_value NOT LIKE '%fr_moderator%'
